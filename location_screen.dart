26828d86d7e2c12c6ccb4d614dc5820fdd5921a1
Jaycse3124 <164001512+Jaycse3124@users.noreply.github.com>
2024-12-01 23:45:25 +0530

    location_screen.dart

--- location_screen.dart
+++ location_screen.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/material.dart';
 
 class LocationScreen extends StatelessWidget {
-  const LocationScreen({Key? key}) : super(key: key); // Add 'key' parameter
+  const LocationScreen({Key? key}) : super(key: key); 
 
   @override
   Widget build(BuildContext context) {
